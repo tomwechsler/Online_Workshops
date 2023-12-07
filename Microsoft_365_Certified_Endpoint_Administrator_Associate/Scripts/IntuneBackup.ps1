db28270ebb22c390773a47b9491a599e070f9472
Tom Wechsler <tom.wechsler@outlook.com>
2023-12-07 07:02:07 +0100

    new files

---
+++ Microsoft_365_Certified_Endpoint_Administrator_Associate/Scripts/IntuneBackup.ps1
@@ -0,0 +1,34 @@
+﻿#Thank your very much to John Seerden! @jseerden
+ 
+Set-Location c:\
+Clear-Host
+
+#Customize the ExecutionPolicy (absolutely OK for this demo)
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+#Install the Module
+Install-Module -Name Microsoft.Graph.Intune -Verbose -Force -AllowClobber
+
+#Install IntuneBackupAndRestore from the PowerShell Gallery
+Install-Module -Name IntuneBackupAndRestore -Verbose -Force -AllowClobber
+
+#Update the Module
+Update-Module -Name IntuneBackupAndRestore -Verbose
+
+#Import the Module
+Import-Module IntuneBackupAndRestore
+
+#Connect to Microsoft Graph
+Connect-MSGraph
+
+#Create a folder
+New-Item -ItemType Directory -Path C:\Backup\IntuneBackup
+
+#Switch to the folder
+Set-Location C:\Backup\IntuneBackup
+
+#Create the Full-Backup
+Start-IntuneBackup -Path 'C:\Backup\IntuneBackup'
+
+#Let's look at the content
+Get-ChildItem -Path 'C:\Backup\IntuneBackup'
